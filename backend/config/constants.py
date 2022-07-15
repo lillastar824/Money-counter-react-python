7a97ab18f916a7a883d5035da3ca42702274598b
JackOfAll <jackyjin824@gmail.com>
2022-07-15 09:16:54 -0400

    Initial commit

---
+++ backend/config/constants.py
@@ -0,0 +1,8 @@
+from xmlrpc.client import TRANSPORT_ERROR
+
+
+TRANSACTION_TYPE = (
+    ('income', 'Income'),
+    ('expense', 'Expense'),
+)
+TRANSACTION_TYPE_DICT = dict(TRANSACTION_TYPE)
---
+++ frontend/src/components/default/Header.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+import Logo from "../../assets/images/Money-Count-Logo.png";
+import { getUser } from "../../reducks/users/selectors";
+import ProfileHeader from "./ProfileHeader";
+
+const Header = () => {
+  // const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  const user = getUser(selector);
+  const token = user ? user.token : null;
+  const [openModalMenu, setOpenModalMenu] = useState(false);
+
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={Logo} alt="Logo" height="40" width="96" />
+      </Link>
+
+      <div>
+        {token ? (
+          <button
+            onClick={() => setOpenModalMenu(true)}
+            className="sign-out-btn"
+          >
+            {user.name}
+            <span className="pic arrow-down"></span>
+          </button>
+        ) : (
+          <Link to="/sign-up">Sign Up</Link>
+        )}
+        <ProfileHeader
+          user={user}
+          openModalMenu={openModalMenu}
+          setOpenModalMenu={setOpenModalMenu}
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
